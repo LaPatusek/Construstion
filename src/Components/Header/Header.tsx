78459f35dd13e25635ea3021cc1e3af2b1dc8521
LaPatusek <ddrrusek@gmail.com>
2024-02-14 15:28:05 +0100

    main page

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import construstion from '../../Assets/Construction.webp';
 import styles from './Header.module.css';
 
 const Header: React.FC = () => {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={`${styles.header} relative`}>
+      <img src={construstion} alt='' />
+      <div className={styles['header-box']}>
+        <h1>
+          A Better World has to <br /> be Built
+        </h1>
+        <Link to='/kontakt'>Skontaktuj się już dziś</Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,12 +1,89 @@
-import React from 'react';
+import { Global, House, People } from 'iconsax-react';
+import React, { Fragment, useRef } from 'react';
 import Header from '../Components/Header/Header';
+import useObserver from '../Components/hooks/useObserver';
+import styles from './Main.module.css';
 
 const Main: React.FC = () => {
+  const BoxesRef = useRef<HTMLDivElement>(null);
+  const boxIsVis = useObserver(BoxesRef);
+
+  const iconSize = 44;
+
   return (
-    <div>
+    <Fragment>
       <Header />
-      Główna
-    </div>
+      <div className={styles.wrap}>
+        <div className={`${styles['first-section']} grid`}>
+          <div>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse vitae maximus magna. Suspendisse potenti. Integer
+              lacus metus, iaculis non erat id, placerat dictum urna. Duis ut
+              scelerisque leo. Nulla eros augue, sagittis sit amet metus quis,
+              molestie sagittis nibh. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Suspendisse vitae maximus magna.
+            </p>
+          </div>
+        </div>
+        <div className={styles['second-section']}>
+          <h3>Od 2002 w branży</h3>
+          <div className={`${styles['box-wrap']} grid`} ref={BoxesRef}>
+            <div
+              className={`${styles.box} ${
+                boxIsVis ? styles['first-active-box'] : ''
+              }`}
+            >
+              <div>
+                <span className={styles.number} />
+                <h4>
+                  Ukończonych <br /> projektów
+                </h4>
+              </div>
+              <div className={styles.icon}>
+                <Global variant='Bold' size={iconSize} />
+              </div>
+            </div>
+
+            <div
+              className={`${styles.box} ${
+                boxIsVis ? styles['second-active-box'] : ''
+              }`}
+            >
+              <div>
+                <span className={styles.number} />
+                <h4>
+                  Wybudowanych <br /> domów
+                </h4>
+              </div>
+              <div className={`${styles.icon} ${styles['mid-icon']}`}>
+                <House variant='Bold' size={iconSize} />
+              </div>
+            </div>
+
+            <div
+              className={`${styles.box} ${
+                boxIsVis ? styles['third-active-box'] : ''
+              }`}
+            >
+              <div>
+                <span className={styles.number} />
+                <h4>
+                  Zatrudnionych <br /> specjalistów
+                </h4>
+              </div>
+              <div className={styles.icon}>
+                <People variant='Bold' size={iconSize} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles['third-section']}>
+        <h3>Odkryj nasze projekty </h3>
+      </div>
+    </Fragment>
   );
 };
 
